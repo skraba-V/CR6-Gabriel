ed0ff7fc77551c6912bb3be488a36c61b8db439c
skraba-V <gabriel.skraba6@gmail.com>
2022-06-19 13:57:41 +0200

    eddt

--- src/app/dishes.ts
+++ src/app/dishes.ts
@@ -1,22 +1,50 @@
 export const dishes = [{
     name: 'Pizza',
-    img: '',
-    price: 5,
-    description: 'A large phone with one of the best screens',
+    img: 'assets/img/pizzasalami.jpeg',
+    price: 8,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     availability: true
   },
   {
     name: 'Pasta',
-    img: '',
-    price: 2,
-    description: 'A large phone with one of the best screens',
+    img: 'assets/img/pasta.jpeg',
+    price: 6,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     availability: false
   },
   {
-    name: 'Nesto',
-    img: '',
+    name: 'Burger',
+    img: 'assets/img/burger.jpeg',
+    price: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    availability: true
+  },
+  {
+    name: 'Lasagna',
+    img: 'assets/img/lasagna.jpeg',
+    price: 7,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    availability: true
+  },
+  {
+    name: 'Schnitzel',
+    img: 'assets/img/Schnitzel.jpeg',
     price: 8,
-    description: 'A large phone with one of the best screens',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    availability: true
+  },
+  {
+    name: 'Prosciutto',
+    img: 'assets/img/prosciutto.jpeg',
+    price: 9,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    availability: true
+  },
+  {
+    name: 'Gulasch',
+    img: 'assets/img/goulash.jpeg',
+    price: 8,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     availability: true
   }
 ];
